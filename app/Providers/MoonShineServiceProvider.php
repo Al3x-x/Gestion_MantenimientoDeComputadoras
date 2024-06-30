7b3c38e046f0ed87bbb351067263b7de2ad4f050
Alex___x <darwinquezada30@hotmail.com>
2024-06-30 06:16:57 +0000

    Initial commit

---
+++ app/Providers/MoonShineServiceProvider.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use MoonShine\Providers\MoonShineApplicationServiceProvider;
+use MoonShine\MoonShine;
+use MoonShine\Menu\MenuGroup;
+use MoonShine\Menu\MenuItem;
+use MoonShine\Resources\MoonShineUserResource;
+use MoonShine\Resources\MoonShineUserRoleResource;
+use MoonShine\Contracts\Resources\ResourceContract;
+use MoonShine\Menu\MenuElement;
+use MoonShine\Pages\Page;
+use Closure;
+
+class MoonShineServiceProvider extends MoonShineApplicationServiceProvider
+{
+    /**
+     * @return list<ResourceContract>
+     */
+    protected function resources(): array
+    {
+        return [];
+    }
+
+    /**
+     * @return list<Page>
+     */
+    protected function pages(): array
+    {
+        return [];
+    }
+
+    /**
+     * @return Closure|list<MenuElement>
+     */
+    protected function menu(): array
+    {
+        return [
+            MenuGroup::make(static fn() => __('Panel de Control'), [
+                MenuItem::make(
+                    static fn() => __('Administradores'),
+                    new MoonShineUserResource()
+                ),
+                MenuItem::make(
+                    static fn() => __('Roles'),
+                    new MoonShineUserRoleResource()
+                ),
+            ]),
+
+            MenuItem::make('ESPOCH', 'https://www.espoch.edu.ec')
+                ->badge(fn() => 'Check')
+                ->blank(),
+
+            // MenuItem::make(
+            //     static fn() => __('Solicitud de Mantenimiento'), 
+            //     new MoonShineSolicitudMantenimiento()
+            // ),
+        ];
+    }
+
+    /**
+     * @return Closure|array{css: string, colors: array, darkColors: array}
+     */
+    protected function theme(): array
+    {
+        return [];
+    }
+}
